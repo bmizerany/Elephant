9ad768fcb34e0cee175df1f7f23e270f74536ec1
Blake Mizerany <blake.mizerany@gmail.com>
2009-06-04 14:25:48 -0700

    WIP

--- ElephantPreferenceController.m
+++ ElephantPreferenceController.m
@@ -3,7 +3,7 @@
 //  Elephant
 //
 //  Created by blake on 6/4/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//  Copyright 2009 Heroku Inc. All rights reserved.
 //
 
 #import "ElephantPreferenceController.h"
@@ -13,7 +13,8 @@
 
 -(NSString *)category { return @"Logging"; }
 -(NSString *)label { return @"Elephant"; }
--(NSString *)nibName { return @""; }
+-(NSString *)nibName { return @"ElephantPreferences"; }
+
 -(NSImage *)image {
   NSURL *url = [NSURL URLWithString:@"http://www.treehugger.com/happy-elephant-01.jpg"];
   return [[NSImage alloc] initWithContentsOfURL:url];
