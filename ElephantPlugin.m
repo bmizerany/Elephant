76d41c7ece7ea7e726e0ffa099f033366f7ff424
Blake Mizerany <blake.mizerany@gmail.com>
2009-06-03 18:42:20 -0700

    init commit

---
+++ ElephantPlugin.m
@@ -0,0 +1,16 @@
+//
+//  ElephantPlugin.m
+//  Elephant
+//
+//  Created by blake on 6/3/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ElephantPlugin.h"
+
+
+@implementation ElephantPlugin
+
+
+
+@end
